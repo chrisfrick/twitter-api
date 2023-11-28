afca2f357ff802d8e85913f1e02aa1a0cf5724ef
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-11-28 13:01:27 -0500

    create UserController

---
+++ src/main/java/com/cooksys/socialmedia/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.cooksys.socialmedia.controllers;
+
+import com.cooksys.socialmedia.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+}
