c9c1b18e66abc6d19db8a2e739719caf5b8d0db2
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-11-28 12:27:52 -0500

    add unique constraint to username

--- src/main/java/com/cooksys/socialmedia/entities/Credentials.java
+++ src/main/java/com/cooksys/socialmedia/entities/Credentials.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 public class Credentials {
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false)
