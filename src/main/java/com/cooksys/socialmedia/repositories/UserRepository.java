e01360741ea7ea283d9688aea59000b4b5bca150
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-11-29 18:35:37 -0500

    add findByCredentials to user repository

--- src/main/java/com/cooksys/socialmedia/repositories/UserRepository.java
+++ src/main/java/com/cooksys/socialmedia/repositories/UserRepository.java
@@ -1,14 +1,18 @@
 package com.cooksys.socialmedia.repositories;
 
+import com.cooksys.socialmedia.entities.Credentials;
 import com.cooksys.socialmedia.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    public List<User> findAllByDeletedFalse();
+    List<User> findAllByDeletedFalse();
+
+    Optional<User> findByCredentials(Credentials credentials);
 
 }
