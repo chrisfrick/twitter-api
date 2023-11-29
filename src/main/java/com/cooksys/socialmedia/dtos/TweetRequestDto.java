7316752fe93ccb5d332e597ac8710079b144d0f3
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-11-29 18:37:25 -0500

    replace SimpleTweetRequestDto with a unified TweetRequestDto to use for all Tweet requests

--- src/main/java/com/cooksys/socialmedia/dtos/TweetRequestDto.java
+++ src/main/java/com/cooksys/socialmedia/dtos/TweetRequestDto.java
@@ -1,5 +1,15 @@
 package com.cooksys.socialmedia.dtos;
 
-public class TweetRequestDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TweetRequestDto {
+    
+    private String content;
+
+    private CredentialsDto credentials;
 }
